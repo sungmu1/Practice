cb6ba1bf634e634d0b4bfbf81ff6d7f0139f891f
sungmu1 <61034399+sungmu1@users.noreply.github.com>
2021-02-16 11:44:01 +0900

    Create pascal_triangle.c

---
+++ pascal_triangle.c
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int pascal_triangle(int size);
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T; //case 갯수
+    int size;//삼각형의 크기
+	
+	cin>>T;
+    for(test_case = 1; test_case <= T; ++test_case)
+	{
+        cin >> size;
+        cout << "#" << test_case << endl ;
+        pascal_triangle(size);
+	}
+	return 0;//정상종료시 반
+}
+
+int pascal_triangle(int size)
+{
+    int active[10] ;
+    //int *active = malloc(sizeof(int)*size);
+    int save[10];
+    int tmp=0;
+    for(int i=0 ; i<size ; i++)
+    {
+        for(int j=0 ; j<i+1 ; j++)
+        {
+             if( j == i )
+            {    
+                active[j] = 1;
+                cout << 1 << endl;
+            }
+            else if( j  == 0)
+            {
+                active[j] = 1;
+                cout << 1 << " ";
+            }
+            else
+            {
+                tmp += save[j-1];
+                tmp += save[j];
+                active[j] = tmp;
+                cout << tmp << " ";
+                tmp =0;
+            }
+        }
+    		for(int k=0 ; k<i+1 ;k++)
+            {	
+            	save[k] = active[k];
+            }
+    }
+}

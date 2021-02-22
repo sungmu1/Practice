2704a2703807f2e443c881bf49fa3580fedd4719
sungmu1 <61034399+sungmu1@users.noreply.github.com>
2021-02-22 15:44:34 +0900

    Update infinity_dictionary.cpp

--- infinity_dictionary.cpp
+++ infinity_dictionary.cpp
@@ -4,61 +4,66 @@
 
 using namespace std;
 
-bool dic_verification(string word_a, string word_b); //word_a와 word_b가 순차적인 사전의 단어인지 확인하는 함수
-void blank_modify(string word, char* mod_word); //띄어쓰기 되어있는 단어를 띄어쓰기를 수정하는 함수
+bool verification(char* word_a, char* word_b);
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	
 	//freopen("input.txt", "r", stdin);
-    scanf("%d\n",&T); //getline의 \n을 제외하여 입력을 받기 위함
-	//cin>>T;
+	cin>>T;
 	
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
-        string word_a,word_b;
-    	//string word_a = (char *)malloc(sizeof(char)*20);
-    	//string word_b = (char *)malloc(sizeof(char)*20);
-        while(word_a.length() == 0) // 입력값이 공백이 아닐때까지 반복
-        	getline(cin, word_a); //한줄을 통채로 입력받음
-        while(word_b.length() == 0) // 입력값이 공백이 아닐때까지 반복
-        	getline(cin, word_b); //한줄을 통채로 입력받음
-        char* mod_a = (char *) malloc (sizeof(word_a.length()));
-        char* mod_b = (char *) malloc (sizeof(word_b.length()));
-        //cout <<"word_length : " << word_a.length() << word_a << "  " <<word_b <<endl ;
-        blank_modify(word_a, mod_a);
-		blank_modify(word_b, mod_b);
-        //cout <<"mod_length : " << strlen(mod_a) << mod_a << "  " <<mod_b <<endl ;
+    	char* word_a = (char *)malloc(sizeof(char)*10);
+    	char* word_b = (char *)malloc(sizeof(char)*10);
+		cin >> word_a;
+        while(strlen(word_a) == 0)
+			cin >> word_a;
+        cin >> word_b;
+        while(strlen(word_b) == 0)
+        	cin >> word_b;
+        //cout << word_a <<word_b ;
         
-        //cout << "mod_a : " << mod_a << ", ";
-        //cout << "mod_b : " << mod_b << endl;
-        if(dic_verification(mod_a, mod_b))
+        if(verification(word_a, word_b))
             cout << "#" <<test_case << " Y" << endl;
-        else if(!dic_verification(mod_a, mod_b))
+        else if(!verification(word_a, word_b))
             cout << "#" <<test_case << " N" << endl;
         else
-            cout << "#" << test_case << "error : not verificate" << endl;
-        
-        free(mod_a);
-        free(mod_b);
-
+            cout << "Error : not verficate" << endl;
+		free(word_a);
+        free(word_b);
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
 
-bool dic_verification(string word_a, string word_b)
+bool verification(char* word_a, char* word_b)
 {
-    int word_a_length = word_a.length();
-    int word_b_length = word_b.length();
-    for( int i=0 ; i < word_a_length ; i++)//일단 a의 길이만큼 비교
+    int word_a_length = strlen(word_a);
+    int word_b_length = strlen(word_b);
+    int blank_a =0, blank_b=0;
+    int increase_a =0, increase_b=0;
+    for( int i=0 ; i < word_a_length ; i++)//일단 a
     {
-        if(word_a[i] > word_b[i]) //a가 b보다 순서가 뒤면 무조건 오답
+        if(word_a[i+blank_a] == ' ') //공백이 있을경우에는 공백을 제외하고 생각
+        {
+            blank_a++;
+            word_a_length--;
+    	}
+        if(word_b[i+blank_b] == ' ') //공백이 있을경우에는 공백을 제외하고 생각
+        {
+            blank_b ++;
+            word_b_length--;
+        }
+        
+        increase_a = i+blank_a;
+        increase_b = i+blank_b;
+       // cout << increase_a << " "<< word_a[increase_a] << " " <<word_b[increase_b]<<endl;
+        if(word_a[increase_a] > word_b[increase_b])
             return false;
-        else if(word_a[i] < word_b[i])//a가 b보다 순서가 앞이면 무조건 정답
+        else if(word_a[increase_a] < word_b[increase_b])
             return true;
-        else if(word_a[i] == word_b[i]) //계속 같은 단어일 경우에는 계속 이어나감
+        else if(word_a[increase_a] == word_b[increase_b])
             continue;
         else
         {
@@ -66,31 +71,19 @@ bool dic_verification(string word_a, string word_b)
             return false;
         }
     }
-    //a단어의 길이만큼은 같았다면
-    if(word_a_length > word_b_length)
+    //앞에는 a랑 모두 같을경우
+    if(word_a_length > word_b_length) //a가 b보다 길때
         return false;
-    else if(word_a_length == word_b_length)
+    else if(word_a_length == word_b_length) //a랑 b가 길이가 같을 때
         return false;
-    else
+    else //a보다 b가 길때
     {
-        for(int i=word_a_length ; i<word_b_length ; i++)
-        {
-            if(word_b[i] !='a')
-            	return true;
-        }
-        return false;
-    }
-}
-
-void blank_modify(string word, char* mod_word)
-{
-    int word_length = word.length();
-    short blank_count =0;
-    
-    for(int i=0 ; i < word_length ; i++)
-    {
-        if( word[i+blank_count] == ' ' )
-            blank_count ++;
-       mod_word[i] = word[i+blank_count];
+        if(word_a_length+1 < word_b_length) //b가 a보다 글자가 2개 더 많을 때
+            return true;
+        //b가 a보다 글자가 1개 더 많을 때
+        if(word_b[word_a_length] !='a') //1개가 a가 아닐때
+          	return true;
+        else if(word_b[word_a_length] == 'a') // 1개가 a일때
+        	return false;
     }
 }
